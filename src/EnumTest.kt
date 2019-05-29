e6ddd379ebaba22a1261df5658ee001d62799ec0
KailaashJeevan <kailaash80@gmail.com>
2019-05-30 01:12:08 +0530

    An ENUM FILE HAS BEEN ADDED...

---
+++ src/EnumTest.kt
@@ -0,0 +1,20 @@
+
+
+
+enum class directions{
+    NORTH,SOUTH,EAST,WEST
+
+}
+
+fun main(){
+    var dir=directions.WEST
+    if(dir==directions.EAST)
+    {
+        println("HE WENT TO $dir")
+
+
+    }
+    else{
+        println("IDK WHERE HE WENT!")
+    }
+}
